b051643a57302d7528d1350f552ced836c4186da
Younes <younes@interactive-object.com>
2012-07-03 19:25:17 +0200

    add cursor synchro

--- server.js
+++ server.js
@@ -20,6 +20,53 @@ server.get('/', function(req, res){
 	res.render('index.ejs', {locals:{mes:mes}});
 });
 
+
+
+var io = require('socket.io');
+var _ = require('underscore')._;
+
+io = io.listen(server);
+io.configure(function(){
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+  io.set("close timeout", 10);
+  io.set("log level", 1);
+})
+
+var users = {};
+
+io.sockets.on('connection', function (socket) {
+  var user;
+
+  socket.emit('users-connected', _.values(users));
+
+  socket.on('new-user', function (data) {
+    user = data
+    users[user.id] = data
+    socket.broadcast.emit('user-connected', data);
+  });
+
+  socket.on('message', function(msgInfo){
+    msgInfo.isFromMe = false;
+    socket.broadcast.emit('receive-message', msgInfo);
+  });
+
+  socket.on('mousemove', function (data) {
+    user.cursor = data
+    socket.broadcast.emit('receive-mousemove', user);
+  });
+  
+  socket.on('disconnect', function(){
+    if(user){
+      delete users[user.id];
+      socket.broadcast.emit('user-disconnected', user);
+    };
+  })
+  
+  
+});
+
+
 //this line is necessary for heroku
 var port = process.env.PORT || 3003;
 server.listen(port);
