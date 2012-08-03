31a6ebc7c9a785950c4ea84a3f0933a1a337e250
Younes <younes@interactive-object.com>
2012-08-03 18:23:44 +0200

    update script for inkart

--- public/lab/streetart/script.js
+++ public/lab/streetart/script.js
@@ -1,6 +1,11 @@
 
-
+var socket;
 var users = [];
+var user = {
+      draw: []
+    }
+
+  
 
 var drawbuffer = [];
 
@@ -14,7 +19,6 @@ var mouse = new Point();
 var canvas, localcanvas;
 var context, lcontext;
 
-var ldrawing = [];
 var rdrawing = [];
 
 var color = "rgb(0, 0, 0)";
@@ -31,61 +35,48 @@ var INK_AMOUNT = 20;
 var SPLASH_RANGE = 10;
 var SPLASH_INK_SIZE = 5;
 
-
-
 $(document).ready(function () {
-	
-	if (Modernizr.canvas) {
-		
-		canvas = document.getElementById('canvas');
-   
-    
-		localcanvas = document.getElementById('local-canvas');
-		
-		// get the context
-		context = canvas.getContext('2d');
-		context.clear = function () {
-			this.clearRect(0, 0, canvas.width, canvas.height);
-		}
-		
-		lcontext = localcanvas.getContext('2d');
-		lcontext.clear = function () {
-			this.clearRect(0, 0, localcanvas.width, localcanvas.height);
-		}
-		
-		resizeCanvas();
-		
-		// resize the canvas to fill browser window dynamically
-		window.addEventListener('resize', resizeCanvas, false);
-		
-		document.onmousedown = onDocumentMouseDown;
-		document.onmouseup = onDocumentMouseUp;
-		document.onmousemove = onDocumentMouseMove;
-		
-		brush = new Brush(DEFAULT_BRUSH_SIZE, INK_AMOUNT, SPLASH_RANGE, SPLASH_INK_SIZE);
-		
-		// start draw loop
-		setInterval(loop, 1000 / 60);
-	}
-	
-	// sockets
-	var socket = io.connect('http://');
-	
-  
-  socket.on('error', function (reason){
-    console.error('Unable to connect Socket.IO', reason);
-  });
+	init();
+});
 
+function init() {
+	// Background
+	$.backstretch("bg.jpg", {
+		speed : 500
+	},
+		connect);
+	
+}
+
+function connect() {
+	// sockets
+	socket = io.connect('http://');
+	
+	socket.on('error', function (reason) {
+		console.error('Unable to connect Socket.IO', reason);
+	});
+	
 	socket.on('connect', function () {
 		connected = true;
 		clearTimeout(timeout);
-    });
+		
+		initUI();
+	});
 	
 	socket.on('disconnect', function () {
 		connected = false;
 		console.log('disconnected');
 		//content.html("<b>Disconnected! Trying to automatically to reconnect in " +  RETRY_INTERVAL/1000 + " seconds.</b>");
 		retryConnectOnFailure(RETRY_INTERVAL);
+	});
+	
+	socket.on('me', function (data) {
+    user = data;
+    if(data.draw)
+    {
+      redraw_with_interval(lcontext, user.draw);
+    }
+    
 	});
 	
 	socket.on('join', function (data) {
@@ -110,7 +101,7 @@ $(document).ready(function () {
 	});
 	
 	socket.on('leave', function (data) {
-
+		
 		var user = users[data.id]
 			if (user) {
 				
@@ -125,182 +116,222 @@ $(document).ready(function () {
 	});
 	
 	socket.on('receive-draw', function (data) {
+    
+    console.log("receive-draw", data);
+    
 		rdrawing.push(data);
 		redraw(context, rdrawing);
 	});
 	
-	socket.on('receive-draw-bulk', function (data) {
-		rdrawing = data;
-	});
-	
-	socket.on('receive-draw-clear', function () {
+	socket.on('user-clear', function () {
 		rdrawing = [];
 		context.clear();
-		lcontext.clear();
 	});
 	
-	// TODO register id in cookie
-	//socket.emit("user");
-  
 	
-	function onDocumentMouseMove(e) {
-		
-		// update the mouse position
-		mouse.set(e.clientX, e.clientY);
-		
-		// send the cursor coordinate
-		socket.emit('mousemove', {
-			x : e.pageX / $(window).width(),
-			y : e.pageY / $(window).height()
+}
+
+function onDocumentMouseMove(e) {
+	
+	// update the mouse position
+	mouse.set(e.clientX, e.clientY);
+	
+	// send the cursor coordinate
+	socket.emit('mousemove', {
+		x : e.pageX / $(window).width(),
+		y : e.pageY / $(window).height()
+	});
+	
+}
+
+function onDocumentMouseDown(e) {
+	brush.reset();
+	isMouseDown = true;
+}
+
+function onDocumentMouseUp(e) {
+	user.draw.push({
+		path : drawbuffer,
+		color : color,
+		size : strokeSize
+	});
+	
+	socket.emit('draw', {
+		path : drawbuffer,
+		color : color,
+		size : strokeSize,
+    user_id : user.user_id
+	});
+	
+	drawbuffer = [];
+	isMouseDown = false;
+}
+
+// main loop for drawing
+function loop() {
+	brush.position(mouse);
+	if (isMouseDown) {
+		// draw the brush on local canvas first
+		brush.draw(lcontext, mouse, color, strokeSize);
+		drawbuffer.push({
+			x : mouse.x,
+			y : mouse.y
 		});
-		
 	}
+}
+
+function setCursor(id, x, y) {
+	var pageX = x * $(window).width();
+	var pageY = y * $(window).height();
+	$("#cursor-" + id).css({
+		left : pageX,
+		top : pageY
+	});
+}
+
+function resizeCanvas() {
 	
-	function onDocumentMouseDown(e) {
+	clearTimeout(resizeIntervalId);
+	resizeIntervalId = setTimeout(doneResizing, 500);
+}
+
+function doneResizing() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	
+	localcanvas.width = window.innerWidth;
+	localcanvas.height = window.innerHeight;
+	
+	redraw_all();
+}
+
+function redrawAllWithInterval() {
+	redraw_with_interval(lcontext, user.draw);
+	redraw_with_interval(context, rdrawing);
+}
+
+function redraw_all() {
+	redraw(lcontext, user.draw);
+	redraw(context, rdrawing);
+}
+
+function redraw(ctx, data) {
+	// redraw stuff
+	
+	console.log("recover start strokeSize", strokeSize);
+	var i = 0;
+	var j = 0;
+	
+	var brush = new Brush(DEFAULT_BRUSH_SIZE, INK_AMOUNT, SPLASH_RANGE, SPLASH_INK_SIZE);
+	
+	for (i = 0; i < data.length; i++) {
+		brush.position(data[i].path[j]);
 		brush.reset();
-		isMouseDown = true;
-	}
-	
-	function onDocumentMouseUp(e) {
-		ldrawing.push({
-			path : drawbuffer,
-			color : color,
-			size : strokeSize
-		});
-		
-		socket.emit('draw', {
-			path : drawbuffer,
-			color : color,
-			size : strokeSize
-		});
-		
-		drawbuffer = [];
-		isMouseDown = false;
-	}
-	
-	// main loop for drawing
-	function loop() {
-		brush.position(mouse);
-		if (isMouseDown) {
-			// draw the brush on local canvas first
-			brush.draw(lcontext, mouse, color, strokeSize);
-			drawbuffer.push({
-				x : mouse.x,
-				y : mouse.y
-			});
-		}
-	}
-	
-	function setCursor(id, x, y) {
-		var pageX = x * $(window).width();
-		var pageY = y * $(window).height();
-		$("#cursor-" + id).css({
-			left : pageX,
-			top : pageY
-		});
-	}
-	
-	function resizeCanvas() {
-		
-		clearTimeout(resizeIntervalId);
-		resizeIntervalId = setTimeout(doneResizing, 500);
-	}
-	
-	function doneResizing() {
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-		
-		localcanvas.width = window.innerWidth;
-		localcanvas.height = window.innerHeight;
-		
-		redraw_all();
-	}
-	
-	function redrawAllWithInterval() {
-		redraw_with_interval(lcontext, ldrawing);
-		redraw_with_interval(context, rdrawing);
-	}
-	
-	function redraw_all() {
-		redraw(lcontext, ldrawing);
-		redraw(context, rdrawing);
-	}
-	
-	function redraw(ctx, data) {
-		// redraw stuff
-		
-		console.log("recover start strokeSize", strokeSize);
-		var i = 0;
-		var j = 0;
-		
-		var brush = new Brush(DEFAULT_BRUSH_SIZE, INK_AMOUNT, SPLASH_RANGE, SPLASH_INK_SIZE);
-		
-		for (i = 0; i < data.length; i++) {
+		for (j = 0; j < data[i].path.length; j++) {
 			brush.position(data[i].path[j]);
-			brush.reset();
-			for (j = 0; j < data[i].path.length; j++) {
+			brush.draw(ctx, data[i].path[j], data[i].color, data[i].size);
+		}
+		j = 0;
+	}
+}
+
+function redraw_with_interval(ctx, data) {
+	// redraw stuff
+	clearInterval(drawIntervalId);
+	
+	var i = 0;
+	var j = 0;
+	
+	var brush = new Brush(DEFAULT_BRUSH_SIZE, INK_AMOUNT, SPLASH_RANGE, SPLASH_INK_SIZE);
+	drawIntervalId = setInterval(function () {
+			
+			if (i < data.length) {
+				
+				// reset brush
+				if (j == 0) {
+					
+					console.log("position with (line, position)=(" + i + "," + j + ")");
+					brush.position(data[i].path[j]);
+					brush.reset();
+					
+				}
 				brush.position(data[i].path[j]);
 				brush.draw(ctx, data[i].path[j], data[i].color, data[i].size);
-			}
-			j = 0;
-		}
-	}
-	
-	function redraw_with_interval(ctx, data) {
-		// redraw stuff
-		clearInterval(drawIntervalId);
-		
-		var i = 0;
-		var j = 0;
-		
-		var brush = new Brush(DEFAULT_BRUSH_SIZE, INK_AMOUNT, SPLASH_RANGE, SPLASH_INK_SIZE);
-		drawIntervalId = setInterval(function () {
 				
-				if (i < data.length) {
-					
-					// reset brush
-					if (j == 0) {
-						
-						console.log("position with (line, position)=(" + i + "," + j + ")");
-						brush.position(data[i].path[j]);
-						brush.reset();
-						
-					}
-					brush.position(data[i].path[j]);
-					brush.draw(ctx, data[i].path[j], data[i].color, data[i].size);
-					
-					if (j < data[i].path.length - 1) {
-						j++
-					} else if (++i < data.length) {
-						j = 0;
-					} else {
-						clearInterval(drawIntervalId);
-						return;
-					}
-					
+				if (j < data[i].path.length - 1) {
+					j++
+				} else if (++i < data.length) {
+					j = 0;
 				} else {
 					clearInterval(drawIntervalId);
 					return;
 				}
 				
-			}, 1000 / 60);
-		
-	}
-	
-	var retryConnectOnFailure = function (retryInMilliseconds) {
-		setTimeout(function () {
-			if (!connected) {
-				$.get('/ping', function (data) {
-					connected = true;
-					window.location.href = unescape(window.location.pathname);
-				});
-				retryConnectOnFailure(retryInMilliseconds);
+			} else {
+				clearInterval(drawIntervalId);
+				return;
 			}
-		}, retryInMilliseconds);
+			
+		}, 1000 / 60);
+	
+}
+
+var retryConnectOnFailure = function (retryInMilliseconds) {
+	setTimeout(function () {
+		if (!connected) {
+			$.get('/ping', function (data) {
+				connected = true;
+				window.location.href = unescape(window.location.pathname);
+			});
+			retryConnectOnFailure(retryInMilliseconds);
+		}
+	}, retryInMilliseconds);
+}
+
+// UI
+
+
+function initUI() {
+    
+	if (Modernizr.canvas) {
+		
+		canvas = document.getElementById('canvas');
+		
+		localcanvas = document.getElementById('local-canvas');
+		
+		// get the context
+		context = canvas.getContext('2d');
+		context.clear = function () {
+			this.clearRect(0, 0, canvas.width, canvas.height);
+		}
+		
+		lcontext = localcanvas.getContext('2d');
+		lcontext.clear = function () {
+			this.clearRect(0, 0, localcanvas.width, localcanvas.height);
+		}
+		
+		// resize the canvas to fill browser window dynamically
+		window.addEventListener('resize', resizeCanvas, false);
+		
+    
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    localcanvas.width = window.innerWidth;
+    localcanvas.height = window.innerHeight;
+  
+  
+		document.onmousedown = onDocumentMouseDown;
+		document.onmouseup = onDocumentMouseUp;
+		document.onmousemove = onDocumentMouseMove;
+		
+		brush = new Brush(DEFAULT_BRUSH_SIZE, INK_AMOUNT, SPLASH_RANGE, SPLASH_INK_SIZE);
+		
+		// start draw loop
+		setInterval(loop, 1000 / 60);
 	}
 	
-	// UI
-	
+
 	// Tools
 	$("#nav #dots ul li a").click(function (e) {
 		
@@ -310,14 +341,6 @@ $(document).ready(function () {
 			$(this).removeClass("cur");
 		});
 		$(e.target).addClass("cur");
-	});
-	
-	// Background
-	$.backstretch("bg.jpg", {
-		speed : 500
-	},
-		function () {
-		redrawAllWithInterval();
 	});
 	
 	// Info
@@ -331,19 +354,15 @@ $(document).ready(function () {
 	// clear button
 	$("#nav .clear").click(function () {
 		
-		socket.emit('draw-clear');
-		
-		ldrawing = [];
-		rdrawing = [];
+		socket.emit('clear');
+		user.draw = [];
 		
 		if (lcontext) {
 			lcontext.clear();
 		}
-		if (context) {
-			context.clear();
-		}
+		
 		clearInterval(drawIntervalId);
 		
 	});
 	
-});
+}
--- server.js
+++ server.js
@@ -1,228 +1,223 @@
-var express = require('express')
-  , cookie = require('cookie')
-  , connect = require('connect')
-  , app = express()
-  , http    = require('http');
+var express = require('express'), cookie = require('cookie'), connect = require('connect'), app = express(), http = require('http'), RedisStore = require('connect-redis')(express);
 
-
-  
-const COOKIE_SECRET= "dfsiumzzbmyblmglwxwc";
+const COOKIE_SECRET = "dfsiumzzbmyblmglwxwc";
+cookie.secret = COOKIE_SECRET;
 
 // redis
-var redis = require('redis-url').connect(process.env.REDISTOGO_URL|| "redis://localhost:6379");
-redis.del("drawdata"); // clear data
+var redis = require('redis-url').connect(process.env.REDISTOGO_URL || "redis://localhost:6379");
+redis.on("error", function (err) {
+	console.log("Error " + err);
+});
 
+redis.del("drawdata"); // clear data
 
 
 // web server
 
-app.configure(function(){
-    
-  var oneYear = 31557600000;
-  this.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  
-  // Session management
-  // Internal session data storage engine, this is the default engine embedded with connect.
-  // Much more can be found as external modules (Redis, Mongo, Mysql, file...). look at "npm search connect session store"
-  this.sessionStore = new express.session.MemoryStore({ reapInterval: 60000 * 10 });
-  
-  // Allow parsing cookies from request headers
-  this.use(express.cookieParser());
-  // Session management
-  this.use(express.session({
-    // Private crypting key
-    "secret": COOKIE_SECRET,
-    "store":  this.sessionStore  
-  }));
-  
-  // Allow parsing form data
-  this.use(express.bodyParser());
-  
+app.configure(function () {
+	
+	var oneYear = 31557600000;
+	this.use(express.static(__dirname + '/public', {
+			maxAge : oneYear
+		}));
+	
+	this.redisStore = new RedisStore();
+	
+	// Allow parsing cookies from request headers
+	this.use(express.cookieParser(COOKIE_SECRET));
+	// Session management
+	this.use(express.session({
+			store : this.redisStore
+		}));
+	
+	// Allow parsing form data
+	this.use(express.bodyParser());
+	
 });
 
-
-app.configure('development', function(){
-  this.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function () {
+	this.use(express.errorHandler({
+			dumpExceptions : true,
+			showStack : true
+		}));
 });
 
-app.configure('production', function(){
-  this.use(express.errorHandler());
+app.configure('production', function () {
+	this.use(express.errorHandler());
 });
-
-
 
 var ejs = require('ejs');
 app.set('view engine', 'ejs');
-app.set('view options', { layout: false });
+app.set('view options', {
+	layout : false
+});
 app.set('views', __dirname + '/views');
 
-
-app.get('/', function(req, res){
-    
-   
-    
-  if (!req.session.user_id) {
-    req.session.user_id = Math.round(Math.random() * 10000);
-    console.log("new session.user_id=",  req.session.user_id);
-  }else {
-     console.log("session user_id=", req.session.user_id);
-  }
+app.get('/', function (req, res) {
+	
+	if (!req.session.user_id) {
+		req.session.user_id = Math.round(Math.random() * 10000);
+		req.session.save();
+		console.log("new user_id=", req.session.user_id);
+	} else {
+		console.log("found user_id=", req.session.user_id);
+	}
 	res.render('index.ejs');
 });
 
-
-app.get('/lab/*', function(req, res){
-  res.render('index.ejs');
+app.get('/lab/*', function (req, res) {
+	res.render('index.ejs');
 });
 
-app.get('/ping', function(req, res) {
-    res.send('pong');
+app.get('/ping', function (req, res) {
+	res.send('pong');
 });
-
-
 
 // socket.io
 var server = http.createServer(app);
-var io = require('socket.io').listen(server, { log: true });
-
+var io = require('socket.io').listen(server, {
+		log : true
+	});
 
 var _ = require('underscore')._;
 var users = {};
 
-io.configure(function(){
-    
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-  io.set("close timeout", 10);
-  io.set("log level", 1);
- 
-
-  io.set('authorization', function (handshakeData, callback) {
-      
-     // Read cookies from handshake headers
-     var cookies = cookie.parse(decodeURIComponent(handshakeData.headers.cookie));
-     //var cookies = connect.utils.parseSignedCookies(cookie.parse(decodeURIComponent(handshakeData.headers.cookie)),COOKIE_SECRET);
-     
-     // We're now able to retrieve session ID
-     var sessionID = cookies['connect.sid'];
-     
-     console.log("handshakeData.headers.cookie="+handshakeData.headers.cookie);
-     console.log("cookies="+cookies['user_id']);
-     
-     // No session? Refuse connection
-    if (!sessionID) {
-      callback('No session', false);
-    } else {
-      // Store session ID in handshake data, we'll use it later to associate
-      // session with open sockets
-      handshakeData.sessionID = sessionID;
-      
-      app.sessionStore.get(sessionID, function (err, session) {
-        
-        if (!err && session && session.user_id) {
-          // On stocke ce username dans les données de l'authentification, pour réutilisation directe plus tard
-          handshakeData.user_id = session.user_id;
-          // OK, on accepte la connexion
-          callback(null, true);
-        } else {
-           console.log("sessionStore.get error="+err);
-          // Session incomplète, ou non trouvée
-          callback(err || 'User not authenticated', false);
-        }
-        
-      });
-      
-    }
-  });
-
-
+io.configure(function () {
+	
+	io.set("transports", ["xhr-polling"]);
+	io.set("polling duration", 10);
+	io.set("log level", 1);
+	
+	io.set('authorization', function (handshakeData, callback) {
+		
+		// Read cookies from handshake headers
+		var cookies = cookie.parse(decodeURIComponent(handshakeData.headers.cookie));
+		//var cookies = connect.utils.parseSignedCookies(cookie.parse(decodeURIComponent(handshakeData.headers.cookie)),COOKIE_SECRET);
+		
+		// We're now able to retrieve session ID
+		var sessionID = cookies['connect.sid'].split('.')[0].split(':')[1];
+		
+		console.log("sessionID=" + sessionID);
+		
+		// No session? Refuse connection
+		if (!sessionID) {
+			callback('No session', false);
+		} else {
+			// Store session ID in handshake data, we'll use it later to associate
+			// session with open sockets
+			
+			
+			app.redisStore.get(sessionID, function (err, session) {
+				
+				console.log("app.sessionStore.get user_id=" + session.user_id);
+				
+				if (!err && session && session.user_id) {
+					// On stocke ce username dans les données de l'authentification, pour réutilisation directe plus tard
+					handshakeData.user_id = session.user_id;
+					handshakeData.sessionID = sessionID;
+					
+					// OK, on accepte la connexion
+					callback(null, true);
+					
+				} else {
+					console.log("sessionStore.get error=" + err);
+					// Session incomplète, ou non trouvée
+					callback(err || 'User not authenticated', false);
+				}
+				
+			});
+			
+		}
+	});
+	
 })
 
 io.sockets.on('connection', function (socket) {
-  var user = {}
-  var has_draw = false;
+	var user = {}
+	var has_draw = false;
+	
+	
+	
+	redis.get("user_" + socket.handshake.user_id, function (err, data) {
+		
+		if (!data) {
+      
+			user.cursor = {
+				x : 0,
+				y : 0
+			}
+			user.id = socket.handshake.user_id;
+      user.draw = [];
+      
+			// storing new user
+			redis.set("user_" + socket.handshake.user_id, JSON.stringify(user));
+      
+		} else {
+      
+      // parse existing data
+			user = JSON.parse(data);
+      // get user draw info
+      
+			redis.get("user_" + user.id + "_draw", function (err, data) {
+				if (data) {
+					has_draw = true;
+					try {
+						var draw = JSON.parse("[" + data + "]")
+            user.draw = draw;
+            
+					} catch (SyntaxError) {
+						return false;
+					}
+				}
+        socket.broadcast.emit('join', user);
+        socket.emit('me', user);
+			});
+			
+		}
+    
+    // keep user id into memory for easy access
+    users[user.id] = user
+		
+	});
+	
   
   socket.emit('users', _.values(users));
   
   
-  redis.get("drawdata" , function(err, data) {
-    if(data){
-     has_draw = true;
-      try { 
-        var drawdata = JSON.parse("["+data+"]") 
-      }
-      catch (SyntaxError) { return false; }
-      
-      socket.emit('receive-draw-bulk', drawdata);
-    }
-  });
-
-  
-  redis.get("user:"+socket.handshake.user_id , function(err, data) {
-
-    user = {}
-    if(!data)
-    {
-      user.cursor = {x:0,y:0}
-      user.id = socket.handshake.user_id;
-      
-      // storing new user 
-      console.log("storing new user data=",user.id);
-      redis.set("user:"+socket.handshake.user_id, user);
-    }else {
-      user = data;
-      console.log("user data found data=",user.id);
-    }
-    
-    // keep user id into memory for easy access
-    users[user.id] = user
-    socket.broadcast.emit('join', user);
-    
-  });
-  
-  socket.on('message', function(msgInfo){
-    msgInfo.isFromMe = false;
-    socket.broadcast.emit('receive-message', msgInfo);
-  });
-
-  socket.on('mousemove', function (data) {
-    user.cursor = data
-    socket.broadcast.emit('receive-mousemove', user);
-  });
-  
-  
-  socket.on('draw', function (data) {
-    socket.broadcast.emit('receive-draw', data);
-    if(has_draw)
-    {
-      redis.append('drawdata', ","+JSON.stringify(data));
-    }else {
-      redis.set('drawdata',JSON.stringify(data));
-      has_draw = true;
-    }
-  });
-  
-  socket.on('draw-clear', function(data){
-    redis.del('drawdata');
-    has_draw = false;
-    socket.broadcast.emit('receive-draw-clear');
-  });
-  
-  socket.on('disconnect', function(){
-    if(user){
-      delete users[user.id];
-      socket.broadcast.emit('leave', user);
-    };
-  })
-  
-  
+	socket.on('message', function (msgInfo) {
+		msgInfo.isFromMe = false;
+		socket.broadcast.emit('receive-message', msgInfo);
+	});
+	
+	socket.on('mousemove', function (data) {
+		user.cursor = data
+			socket.broadcast.emit('receive-mousemove', user);
+	});
+	
+	socket.on('draw', function (data) {
+		socket.broadcast.emit('receive-draw', data);
+		if (has_draw) {
+			redis.append("user_" + user.id + "_draw", "," + JSON.stringify(data));
+		} else {
+			redis.set("user_" + user.id + "_draw", JSON.stringify(data));
+			has_draw = true;
+		}
+	});
+	
+	socket.on('clear', function (data) {
+		redis.del("user_" + user.id + "_draw");
+		has_draw = false;
+		socket.broadcast.emit('user-clear');
+	});
+	
+	socket.on('disconnect', function () {
+		if (user) {
+			delete users[user.id];
+			socket.broadcast.emit('leave', user);
+		};
+	})
+	
 });
 
-
 //this line is necessary for heroku
-server.listen( process.env.PORT || 3003 );
-
-
-
-
-
+server.listen(process.env.PORT || 3003);
